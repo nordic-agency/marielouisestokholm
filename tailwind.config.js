35468da76f70afcfc31d7a1395fe15268280c91a
Mads Christensen <madschristensen@madss-mbp-3.lan>
2025-08-06 09:40:24 +0200

    Din beskrivelse her

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        slide: 'slide 40s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
